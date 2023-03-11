e07f20aa49525aa4563a32db32b46bd9d0117c69
Digvijay Anand <digvijayananddevelopment@gmail.com>
2023-03-12 02:28:10 +0530

    adding header to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ import obj from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import Header from './components/header/header.component';
+
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<obj.home />} />
         <Route path='/shop' element={<ShopPage />} />
